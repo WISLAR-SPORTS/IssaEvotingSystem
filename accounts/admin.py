d8cc34db1fcc086d4c16558ab850e3f0e2309a53
WISLAR SPORTS <wamaniissa0@gmail.com>
2026-05-07 20:18:50 +0300

    admin  fix

--- accounts/admin.py
+++ accounts/admin.py
@@ -19,15 +19,52 @@ class PasswordResetOTPAdmin(admin.ModelAdmin):
         return obj.user.email
 
     get_email.short_description = "Email"
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
 
 @admin.register(User)
 class CustomUserAdmin(BaseUserAdmin):
 
-    list_display = ("username","email","student_number","current_session_key","phone_number","first_name","last_name", "role", "institution", "branch")
+    list_display = (
+        "username",
+        "email",
+        "student_number",
+        "current_session_key",
+        "phone_number",
+        "first_name",
+        "last_name",
+        "role",
+        "institution",
+        "branch",
+    )
 
+    # ✅ EDIT USER PAGE
     fieldsets = BaseUserAdmin.fieldsets + (
         ("Institution Info", {
-            "fields": ("role", "institution", "branch")
+            "fields": (
+                "role",
+                "institution",
+                "branch",
+                "student_number",
+                "phone_number",
+                "current_session_key",
+            )
+        }),
+    )
+
+    # ✅ ADD USER PAGE (THIS WAS MISSING)
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ("Institution Info", {
+            "classes": ("wide",),
+            "fields": (
+                "role",
+                "institution",
+                "branch",
+                "student_number",
+                "phone_number",
+            ),
         }),
     )
 
@@ -51,27 +88,35 @@ class CustomUserAdmin(BaseUserAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
 
         if request.user.is_superuser:
-         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+            return super().formfield_for_foreignkey(
+                db_field,
+                request,
+                **kwargs
+            )
 
-    # Institution admin scope
+        # Institution admin scope
         if request.user.role == "institution_admin":
 
-         if db_field.name == "institution":
-            kwargs["queryset"] = db_field.related_model.objects.filter(
-                id=request.user.institution_id
-            )
+            if db_field.name == "institution":
+                kwargs["queryset"] = db_field.related_model.objects.filter(
+                    id=request.user.institution_id
+                )
 
-    # Branch admin scope
+        # Branch admin scope
         if request.user.role == "branch_admin":
 
-         if db_field.name == "institution":
-            kwargs["queryset"] = db_field.related_model.objects.filter(
-                id=request.user.institution_id
-            )
+            if db_field.name == "institution":
+                kwargs["queryset"] = db_field.related_model.objects.filter(
+                    id=request.user.institution_id
+                )
 
-        if db_field.name == "branch":
-            kwargs["queryset"] = db_field.related_model.objects.filter(
-                id=request.user.branch_id
-            )
+            if db_field.name == "branch":
+                kwargs["queryset"] = db_field.related_model.objects.filter(
+                    id=request.user.branch_id
+                )
 
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+        return super().formfield_for_foreignkey(
+            db_field,
+            request,
+            **kwargs
+        )
